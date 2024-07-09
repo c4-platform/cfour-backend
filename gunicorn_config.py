a276dd84c08210142eead2a5e85d20049bb83094
Mohammad Mahdi Moghaddam <mahdimoghaddam2005@gmail.com>
2024-07-09 16:15:55 +0330

    added gunicorn configuration. added games application to django project

---
+++ gunicorn_config.py
@@ -0,0 +1,1 @@
+import multiprocessingbind = "127.0.0.1:8000"  # Replace with your desired IP and portworkers = multiprocessing.cpu_count() * 2 + 1worker_class = "gthread"user = "mohammad"group = "mohammad"threads = 2timeout = 60
